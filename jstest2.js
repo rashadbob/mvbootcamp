10d1d49e8dddfc3cc4c9d0876fb4d1b8d1009980
rashadbob <ahmadrashad001@gmail.com>
2022-04-26 10:33:54 +0100

    just testing git

---
+++ jstest2.js
@@ -0,0 +1,8 @@
+
+
+let length = ['ENG','MATHS']
+for(let i =0 ;i < 2;i++){
+
+console.log(length[i])
+
+}
---
+++ test2.js
@@ -0,0 +1,6 @@
+let length = ['ENG','MATHS']
+for(let i =0 ;i < 2;i++){
+
+console.log(length[i])
+
+} 
